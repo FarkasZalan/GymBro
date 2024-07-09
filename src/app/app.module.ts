021561bafa8493f48ef947e3ca222d6671d5bfa1
Farkas Zalan <farkasz@modularity.hu>
2024-07-09 21:59:41 +0200

    [#3][Firebase]
    - Firebase bekötése, hosting beállítása

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NbSidebarModule, NbMenuModule, NbDatepickerModule, NbDialogModule, NbWindowModule, NbThemeModule } from '@nebular/theme';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
 
 @NgModule({
   declarations: [
@@ -12,12 +18,19 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
+    // Nebular theme modules for menu
     NbSidebarModule.forRoot(),
     NbMenuModule.forRoot(),
     NbDatepickerModule.forRoot(),
     NbDialogModule.forRoot(),
     NbWindowModule.forRoot(),
-    NbThemeModule.forRoot()
+    NbThemeModule.forRoot(),
+    // Firebase
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    MatDialogModule,
+    AngularFirestoreModule,
+    AngularFireFunctionsModule
   ],
   providers: [
     provideAnimationsAsync()
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCvjW2cdH1BDvndEDWAe06kOQ3MTu8fRts",
+    authDomain: "gymbro-3b91b.firebaseapp.com",
+    projectId: "gymbro-3b91b",
+    storageBucket: "gymbro-3b91b.appspot.com",
+    messagingSenderId: "574168793050",
+    appId: "1:574168793050:web:853a5e2c83805217d00ae3",
+    measurementId: "G-NR802HPTGV"
+  }
+};
